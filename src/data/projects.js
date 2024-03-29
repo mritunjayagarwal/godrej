1f0fab4660d8b6363f7ea286b723ccf57b178d31
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2024-03-30 04:03:40 +0530

    Added Homepage

---
+++ src/data/projects.js
@@ -0,0 +1,86 @@
+const projects = [
+    {
+        "Id": 1,
+        "City": "Noida",
+        "Projects": [
+            {
+                "Id": 1,
+                "Name": "GODREJ TROPICAL ISLE",
+                "Type": "3 & 4 BHK ULTRA LUXURY APARTMENTS",
+                "Price": "₹ 3.20 CR* ONWARDS",
+                "Location": "SECTOR 146, NOIDA"
+            },
+            {
+                "Id": 2,
+                "Name": "GODREJ TROPICAL ISLE",
+                "Type": "3 & 4 BHK ULTRA LUXURY APARTMENTS",
+                "Price": "₹ 3.20 CR* ONWARDS",
+                "Location": "SECTOR 146, NOIDA"
+            },
+            {
+                "Id": 3,
+                "Name": "GODREJ TROPICAL ISLE",
+                "Type": "3 & 4 BHK ULTRA LUXURY APARTMENTS",
+                "Price": "₹ 3.20 CR* ONWARDS",
+                "Location": "SECTOR 146, NOIDA"
+            },
+            {
+                "Id": 4,
+                "Name": "GODREJ TROPICAL ISLE",
+                "Type": "3 & 4 BHK ULTRA LUXURY APARTMENTS",
+                "Price": "₹ 3.20 CR* ONWARDS",
+                "Location": "SECTOR 146, NOIDA"
+            },
+            {
+                "Id": 5,
+                "Name": "GODREJ TROPICAL ISLE",
+                "Type": "3 & 4 BHK ULTRA LUXURY APARTMENTS",
+                "Price": "₹ 3.20 CR* ONWARDS",
+                "Location": "SECTOR 146, NOIDA"
+            }
+        ]
+    },
+    {
+        "Id": 2,
+        "City": "Chandigarh",
+        "Projects": [
+            {
+                "Id": 1,
+                "Name": "GODREJ TROPICAL ISLE",
+                "Type": "3 & 4 BHK ULTRA LUXURY APARTMENTS",
+                "Price": "₹ 3.20 CR* ONWARDS",
+                "Location": "SECTOR 146, NOIDA"
+            },
+            {
+                "Id": 2,
+                "Name": "GODREJ TROPICAL ISLE",
+                "Type": "3 & 4 BHK ULTRA LUXURY APARTMENTS",
+                "Price": "₹ 3.20 CR* ONWARDS",
+                "Location": "SECTOR 146, NOIDA"
+            },
+            {
+                "Id": 3,
+                "Name": "GODREJ TROPICAL ISLE",
+                "Type": "3 & 4 BHK ULTRA LUXURY APARTMENTS",
+                "Price": "₹ 3.20 CR* ONWARDS",
+                "Location": "SECTOR 146, NOIDA"
+            },
+            {
+                "Id": 4,
+                "Name": "GODREJ TROPICAL ISLE",
+                "Type": "3 & 4 BHK ULTRA LUXURY APARTMENTS",
+                "Price": "₹ 3.20 CR* ONWARDS",
+                "Location": "SECTOR 146, NOIDA"
+            },
+            {
+                "Id": 5,
+                "Name": "GODREJ TROPICAL ISLE",
+                "Type": "3 & 4 BHK ULTRA LUXURY APARTMENTS",
+                "Price": "₹ 3.20 CR* ONWARDS",
+                "Location": "SECTOR 146, NOIDA"
+            }
+        ]
+    }
+];
+
+export default projects;
