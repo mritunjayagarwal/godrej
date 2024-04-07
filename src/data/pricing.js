f6602810df5c81da17ae2e0d5a5a839f143493b0
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2024-04-07 05:34:21 +0530

    Added

---
+++ src/data/pricing.js
@@ -0,0 +1,154 @@
+const pricing = [
+    {
+        "Name": "2BHK Refuge - Type 1",
+        "Carprt": {
+            "sqmtr": 77.24,
+            "sqft": 831.4036
+        },
+        "Exclusive": {
+            "sqmtr": 16.87,
+            "sqft": 185.587
+        },
+        "Super": {
+            "sqmtr": 139.3547,
+            "sqft": 1500
+        }
+    },
+    {
+        "Name": "2BHK Refuge - Type 2",
+        "Carprt": {
+            "sqmtr": 77.24,
+            "sqft": 831.4036
+        },
+        "Exclusive": {
+            "sqmtr": 20.18,
+            "sqft": 217.2155
+        },
+        "Super": {
+            "sqmtr": 139.3547,
+            "sqft": 1500
+        }
+    },
+    {
+        "Name": "3BHK - Type 1",
+        "Carprt": {
+            "sqmtr": 90.84,
+            "sqft": 977.7927
+        },
+        "Exclusive": {
+            "sqmtr": 21.38,
+            "sqft": 230.1322
+        },
+        "Super": {
+            "sqmtr": 163.3237,
+            "sqft": 1758
+        }
+    },
+    {
+        "Name": "3BHK - Type 2",
+        "Carprt": {
+            "sqmtr": 90.84,
+            "sqft": 977.7927
+        },
+        "Exclusive": {
+            "sqmtr": 25.34,
+            "sqft": 272.7572
+        },
+        "Super": {
+            "sqmtr": 163.3237,
+            "sqft": 1758
+        }
+    },
+    {
+        "Name": "3BHK+U/ST - Type 1",
+        "Carprt": {
+            "sqmtr": 127.06,
+            "sqft": 1367.661
+        },
+        "Exclusive": {
+            "sqmtr": 32.70,
+            "sqft": 351.9795
+        },
+        "Super": {
+            "sqmtr": 225.9404,
+            "sqft": 2432
+        }
+    },
+    {
+        "Name": "3BHK+U/ST - Type 2",
+        "Carprt": {
+            "sqmtr": 127.06,
+            "sqft": 1367.661
+        },
+        "Exclusive": {
+            "sqmtr": 36.00,
+            "sqft": 387.5004
+        },
+        "Super": {
+            "sqmtr": 225.9404,
+            "sqft": 2432
+        }
+    },
+    {
+        "Name": "3BHK+U/ST - Type 3",
+        "Carprt": {
+            "sqmtr": 143.04,
+            "sqft": 1539.668
+        },
+        "Exclusive": {
+            "sqmtr": 38.80,
+            "sqft": 417.6393
+        },
+        "Super": {
+            "sqmtr": 225.9404,
+            "sqft": 2432
+        }
+    },
+    {
+        "Name": "3BHK+U/ST - Type 4",
+        "Carprt": {
+            "sqmtr": 143.04,
+            "sqft": 1539.668
+        },
+        "Exclusive": {
+            "sqmtr": 42.93,
+            "sqft": 462.0942
+        },
+        "Super": {
+            "sqmtr": 225.9404,
+            "sqft": 2432
+        }
+    },
+    {
+        "Name": "4BHK+U/ST - Type 1",
+        "Carprt": {
+            "sqmtr": 159.26,
+            "sqft": 1714.259
+        },
+        "Exclusive": {
+            "sqmtr": 50.49,
+            "sqft": 543.4693
+        },
+        "Super": {
+            "sqmtr": 299.241,
+            "sqft": 3221
+        }
+    },
+    {
+        "Name": "4BHK+U/ST - Type 1",
+        "Carprt": {
+            "sqmtr": 159.26,
+            "sqft": 1714.259
+        },
+        "Exclusive": {
+            "sqmtr": 60.20,
+            "sqft": 647.9868
+        },
+        "Super": {
+            "sqmtr": 299.241,
+            "sqft": 3221
+        }
+    }
+]
+
+export default pricing;
